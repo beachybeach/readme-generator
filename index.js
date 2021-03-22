aa99905d3c3e1a7d71240fe14f0b8e3d77ce885d
Riley <rsbeach97@gmail.com>
2021-03-21 21:26:37 -0700

    inquirer working, node prompting questions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-cosnt data = () => {
+const data = () => {
   return inquirer.prompt([
     {
       type: 'input',
@@ -18,7 +18,45 @@ cosnt data = () => {
       }
     },
     {
-
+      type: 'input',
+      name: 'description',
+      message: 'Description of the project.',
+      validate: description => {
+        if (description) {
+          return true;
+        } else {
+          console.log('Describe your project!')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'tech',
+      message: 'Tech used in project',
+      validate: tech => {
+        if (tech) {
+          return true;
+        } else {
+          console.log('What tech did you use to build this project?')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Contributors to the project',
+      validate: credits => {
+        if (credits) {
+          return true;
+        } else {
+          console.log('Who helped you build this projects?')
+          return false;
+        }
+      }
     }
-  ])
+  ]);
 }
+
+data();
