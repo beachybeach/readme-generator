43eaff1e00fb45111a193e3ff8076c4f93c117b2
Riley <rsbeach97@gmail.com>
2021-03-21 21:21:36 -0700

    starting inquirer prompts

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+cosnt data = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Title of the project.',
+      validate: title => {
+        if (title) {
+          return true;
+        } else {
+          console.log('What is the name of your project?')
+          return false;
+        }
+      }
+    },
+    {
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
-
-// TODO: Create a function to initialize app
-function init() { }
-
-// Function call to initialize app
-init();
+    }
+  ])
+}
